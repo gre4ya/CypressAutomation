be210d2d7c251f61af8b5d44318cfa87754dccfc
Andrii Grechyn <gre4ya@icloud.com>
2023-04-15 20:04:56 -0500

    added Test1

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,1 +1,18 @@
 //cypress - Spec
+//<reference types= "Cypress" />
+describe('My first Test Suite', function()
+{
+    it('My FirstTest case', function(){
+
+cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+cy.get('.search-keyword').type('ca')
+cy.wait(2000)
+cy.get('.product').should('have.length',4)
+
+    })
+
+
+
+
+
+})
