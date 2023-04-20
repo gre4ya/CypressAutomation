f6d59d094b39e2abfb10c572879fdba9a75a904c
Andrii Grechyn <gre4ya@icloud.com>
2023-04-20 17:41:38 -0500

    added Static and Dynamic dropdown

--- cypress/integration/examples/Test3.js
+++ cypress/integration/examples/Test3.js
@@ -5,11 +5,34 @@ describe('My Second Test Suite', function()
     it('My FirstTest case', function(){
 
         cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+
+
+        // CHECKBOXES
+
         cy.get('#checkBoxOption1').check().should('be.checked').and('have.value','option1')
         cy.get('#checkBoxOption1').uncheck().should('not.be.checked')
 
+        cy.get('input[type="checkbox"]').check(['option2', 'option3']) // check multiple checkboxes
 
+        // DROPDOWNS
 
+        // Static Dropdown
+
+        cy.get('select').select('option2').should('have.value','option2')  // value
+
+        // Dynamic Dropdown
+
+        cy.get('#autocomplete').type('ind')
+
+        cy.get('.ui-menu-item div').each(($el, index, $list) => {
+
+            if($el.text()==='India')
+            {
+                $el.click() 
+            }
+       
+        })
+        cy.get('#autocomplete').should('have.value','India')
     
     })
 
