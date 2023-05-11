2ba028525faacb85e9005fe84c6db2db4dd36db3
Andrii Grechyn <gre4ya@icloud.com>
2023-05-11 16:23:07 -0500

    added CSV download

--- cypress/integration/examples/SessionTest.js
+++ cypress/integration/examples/SessionTest.js
@@ -9,6 +9,25 @@ describe('JWT Session', ()=>{
                     window.localStorage.setItem('token', Cypress.env('token'))
                 }
             })
+
+            cy.get('.card-body button:last-of-type').eq(1).click();
+            cy.get("[routerlink*='cart']").click();
+            cy.contains('Checkout').click();
+
+            cy.get("[placeholder*='Country']").type("ind")
+            cy.get(".ta-results button").each(($el, index, $list) =>
+            {
+                if($el.text().trim() === 'India')
+                {
+                    cy.wrap($el).click();
+                }
+            })
+            cy.get(".action__submit").click();
+            cy.wait(2000);
+            cy.contains('CSV').click();
+
+
+
         })
     })
 })
